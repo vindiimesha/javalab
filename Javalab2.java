2eb407ea97b8de7e2e4bd47739b98c2cece05b7f
vindiimesha <140940443+vindiimesha@users.noreply.github.com>
2023-07-30 23:56:48 +0530

    Update Javalab2.java

--- Javalab2.java
+++ Javalab2.java
@@ -1,1 +1,10 @@
+Package.com.mycompany.informe
+ 
+Public class informe{
+Public static void main(String[] args)
+{
+    system.out.println("your name");
+    system.out.println(" yor degree program");
+}
+}
 
