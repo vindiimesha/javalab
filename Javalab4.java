a4a5afb5cfc16fb75aae4d157f8a9209292780a8
vindiimesha <140940443+vindiimesha@users.noreply.github.com>
2023-07-31 00:00:50 +0530

    Update Javalab4.java

--- Javalab4.java
+++ Javalab4.java
@@ -1,1 +1,16 @@
+      int [] numbers = {10, 20, 30, 40, 50};
+      for(int x : numbers ){
+         if( x == 30 ){
+    break;
+         }
+         System.out.print( x );
+         System.out.print("\n");
+      }
+          System.out.print(“I’m  out of the Loop now");         
 
+Results: 
+10
+20
+30
+40
+50
