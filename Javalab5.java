066144726cda5a2e08526e91d59c486ad7867f73
vindiimesha <140940443+vindiimesha@users.noreply.github.com>
2023-07-31 00:11:24 +0530

    Update Javalab5.java

--- Javalab5.java
+++ Javalab5.java
@@ -1,1 +1,25 @@
+public class GradeChecker {
+    public static void main(String[] args) {
+        char grade = 'A';
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent!");
+                break;
+            case 'D':
+                System.out.println("You passed");
+            case 'F':
+                System.out.println("Better try again");
+                break;
+            default:
+                System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is " + grade);
+    }
+}
+Output:
+Excellent!
+Your grade is A
+You passed
+Better try again
+Your grade is D
 
