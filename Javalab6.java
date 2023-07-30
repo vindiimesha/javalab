a5d8e8c8a101cf2d5c144cfa29406296759a8257
vindiimesha <140940443+vindiimesha@users.noreply.github.com>
2023-07-31 00:05:30 +0530

    Update Javalab6.java

--- Javalab6.java
+++ Javalab6.java
@@ -1,1 +1,27 @@
+class TestEnhanceForLoop {
+public static void mains(String args[]){
+     
+ int [] numbers = {10, 20, 30, 40, 50};
+      
+for(int x : numbers ){
+        
+ System.out.print( x );
+         
+System.out.print(",")
+      
+}
+ System.out.print("\n");
+      
+String [] names ={“James”, "Larry", "Tom", "Lacy"}
+      
+for( String name : names ) {
+        
+ 
+System.out.print( name );
+         
+System.out.print(",");
+     
+   
+Output: 
+
 
